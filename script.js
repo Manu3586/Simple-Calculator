2a833ab5ac2ac0398534e624fc9e62811b810622
Mansi Negi <mansinegi3586@gmail.com>
2025-09-13 20:55:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.btn');
+const clear = document.getElementById('clear');
+const equal = document.getElementById('equal');
+
+let currentInput = '';
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.getAttribute('data-value');
+
+        if (value) {
+            currentInput += value;
+            display.value = currentInput;
+        }
+    });
+});
+
+clear.addEventListener('click', () => {
+    currentInput = '';
+    display.value = '';
+});
+
+equal.addEventListener('click', () => {
+    try {
+        display.value = eval(currentInput);
+        currentInput = display.value;
+    } catch {
+        display.value = 'Error';
+    }
+});
